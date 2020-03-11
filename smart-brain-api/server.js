995259b65c9bb94def0497795c5155dd0951efc7
beboptank <beboptank12@gmail.com>
2020-03-11 14:11:04 +0900

    server and backend updated to work with heroku

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: '###',
-        password: '###',
-        database: 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
@@ -24,7 +22,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('it is working!');
 });
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
@@ -36,8 +34,8 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-    console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
 
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://ancient-peak-84813.herokuapp.com/register', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
